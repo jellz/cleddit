dda0efc29a01ddd81da3ef6bd10e4914ea8bde20
RONTheCookie <ronthecookie0101@gmail.com>
2019-04-13 14:43:16 +0300

    Websocket proxy

--- proxy.js
+++ proxy.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-const proxy = require("express-http-proxy");
+const proxy = require("http-proxy-middleware");
 
-app.use("/api", proxy("http://localhost:3001"))
-app.use("/", proxy("http://localhost:8080"));
+const clientProxy = proxy({ target: "http://localhost:8080", ws: true });
+app.use("/api", proxy({ target: "http://localhost:3001" }));
+app.use("/", clientProxy);
 
-app.listen(port);
+const server = app.listen(port);
 
 setTimeout(() => {
-    console.log(`Listening on:  http://localhost:${port}`)
+    console.log("\n".repeat(5));
+    console.log(`Listening on:  http://localhost:${port}`);
+    console.log("\n".repeat(5));
 }, 1000 * 5);
+
+server.on("upgrade", clientProxy.upgrade);
