4de0d4ac4c1593c4d2ecc1b4978355d97de3f782
RONTheCookie <ronthecookie0101@gmail.com>
2019-04-13 15:19:15 +0300

    new auth endpoints and ratelimit

--- server/api.js
+++ server/api.js
@@ -1,5 +1,15 @@
 const express = require("express");
+const errorHandler = require("api-error-handler");
 const app = (module.exports = express.Router());
+const RateLimit = require("express-rate-limit");
+
+module.exports.ratelimit = new RateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 30
+});
+
+
+app.use(errorHandler());
 app.get("/", (req, res) => res.json({ status: "ok", msg: "hello world" }));
 
 app.use("/auth", require("./routes/auth"));
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,10 +2,21 @@ const express = require("express");
 const app = (module.exports = express.Router());
 const passwordless = require("passwordless");
 const { r } = require("..");
+const { ratelimit } = require("../api");
 app.get("/", (req, res) => res.json({ status: "ok", msg: "hello world auth" }));
+
+app.post("/logout", passwordless.logout(), (req, res) => {
+    res.json({status: "ok"});
+});
+
+app.get("/status", (req, res) => {
+    res.json({status: "ok", auth: req.user ? "user" : "anon"});
+});
+
 
 app.post(
     "/token",
+    ratelimit,
     passwordless.requestToken(async (email, delivery, callback, req) => {
         try {
             const user = await r
@@ -27,6 +38,7 @@ app.post(
 
 app.post(
     "/login",
+    ratelimit,
     passwordless.acceptToken({ allowPost: true }),
     async (req, res) => {
         res.json({status: "ok"});
