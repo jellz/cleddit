34d2e9afa90a6887555603378f27bc6cf1429e3e
jellz <danielgulic@gmail.com>
2019-04-14 15:43:13 +1000

    change route file names

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -2,15 +2,15 @@ import { h, Component } from 'preact';
 import { Router } from 'preact-router';
 
 // Code-splitting is automated for routes
-import Home from '../routes/home';
-import Login from '../routes/login';
-import LoginPostToken from '../routes/login/token';
+import Home from '../routes/home/home.js';
+import Login from '../routes/login/login.js';
+import LoginPostToken from '../routes/login/token/token.js';
 
 export default class App extends Component {
 	
-	handleRouteChange = e => {
-		this.currentUrl = e.url;
-	};
+	// handleRouteChange = e => {
+	// 	this.currentUrl = e.url;
+	// };
 
 	render() {
 		return (
